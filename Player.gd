ed964fbb68d7bd1ff7d64a46206a53209a71cd03
Kamil Borkowski <kamil2000-22@o2.pl>
2020-01-28 20:02:43 +0100

    Character movement physics

--- Player.gd
+++ Player.gd
@@ -2,13 +2,50 @@ extends KinematicBody2D
 
 var motion = Vector2(0,0)
 
-const SPEED = 1000
+const SPEED = 1500
+const GRAVITY = 300
+const UP = Vector2(0,-1)
+const JUMP_SPEED = 5000
 
 func _physics_process(delta):
+	apply_gravity()
+	jump()
+	move()
+	animate()
+	move_and_slide(motion, UP)
+
+
+func apply_gravity():
+	if is_on_floor():
+		motion.y = 0
+	else:
+		motion.y += GRAVITY
+
+
+func jump():
+	if Input.is_action_pressed("jump") and is_on_floor():
+		motion.y -= JUMP_SPEED
+
+
+func move():
 	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
 		motion.x = -SPEED
 	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
 		motion.x = SPEED
 	else:
 		motion.x = 0
-	move_and_slide(motion)
+
+
+func animate():
+	if motion.y < 0:
+		$AnimatedSprite.play("jump")
+	elif motion.x > 0:
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = false
+	elif motion.x < 0:
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = true
+	else:
+		$AnimatedSprite.play("idle")
+
+
